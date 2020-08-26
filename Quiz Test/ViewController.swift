49764a5cfea9cfb645caf2fe3055839901617373
iudexgundir <govoroverx@icloud.com>
2020-08-26 17:41:59 +0300

    Initial Commit

---
+++ Quiz Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz Test
+//
+//  Created by Говоров Эрхаан on 26.08.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
