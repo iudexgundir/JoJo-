232c4ac4872728c8972773985483d88710e65561
iudexgundir <govoroverx@icloud.com>
2020-08-28 18:51:39 +0300

    Create models

---
+++ Quiz Test/Model/Answers .swift
@@ -0,0 +1,12 @@
+//
+//  Answers .swift
+//  Quiz Test
+//
+//  Created by Говоров Эрхаан on 26.08.2020.
+//
+
+struct Answer {
+    var text: String
+    var type: CharacterType
+    
+}
--- Quiz Test/Model/Question.swift
+++ Quiz Test/Model/Question.swift
@@ -5,4 +5,48 @@
 //  Created by Говоров Эрхаан on 26.08.2020.
 //
 
-import Foundation
+struct Question {
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+}
+
+extension Question {
+    static func getQuestions() -> [Question] {
+        return [
+            Question(
+                text: "Что вы цените в людях?",
+                type: .single,
+                answers:
+                    [Answer(text: "Золотой", type: .Giorno),
+                    Answer(text: "Фиолетовый", type: .Joseph),
+                    Answer(text: "Розовый", type: .Josuke),
+                    Answer(text: "Черный", type: .Jotaro),
+                    Answer(text: "Синий", type: .Jonathan)]),
+        
+            Question(
+                text: "Какой цвет вам нравится больше?",
+                type: .multiple,
+                answers:
+                    [Answer(text: "Золотой", type: .Giorno),
+                    Answer(text: "Фиолетовый", type: .Joseph),
+                    Answer(text: "Розовый", type: .Josuke),
+                    Answer(text: "Черный", type: .Jotaro),
+                    Answer(text: "Синий", type: .Jonathan)]),
+            
+            Question(
+                text: "Любите ли вы путешествовать?",
+                type: .ranged,
+                answers:
+                    [Answer(text: "Ненавижу", type: .Josuke),
+                    Answer(text: "Нервничаю", type: .Giorno),
+                    Answer(text: "Не замечаю", type: .Jotaro),
+                    Answer(text: "Люблю", type: .Jonathan),
+                    Answer(text: "Обожаю", type: .Joseph)]),
+        
+            
+            
+            
+        ]
+    }
+}
