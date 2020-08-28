2fa44fe3282a417fc6f52b5e19ca6f326dbb4104
iudexgundir <68199348+iudexgundir@users.noreply.github.com>
2020-08-28 19:04:36 +0300

    Add files via upload
    +response type

---
+++ Quiz Test/Model/ResponseType.swift
@@ -0,0 +1,12 @@
+//
+//  ResponseType.swift
+//  Quiz Test
+//
+//  Created by Говоров Эрхаан on 26.08.2020.
+//  Response types for questions
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
